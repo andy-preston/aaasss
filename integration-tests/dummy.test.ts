b30ae5692d0c292ad6992784581599e1ac0b826b
Andy Preston <edgeeffect@gmail.com>
2024-12-15 19:58:45 +0000

    Mock input and output for integration tests

--- integration-tests/dummy.test.ts
+++ integration-tests/dummy.test.ts
@@ -1,13 +1,27 @@
 import { coupling } from "../src/coupling/coupling.ts";
-import { outputFile } from "../src/output/file.ts";
+
+const mockFileReader = (_path: string | URL): string => {
+    return "dummy file contents";
+};
+
+export const mockOutputFile = (_fileName: string, _extension: string) => {
+    const write = (text: string) => {
+        console.log(text);
+    };
+    const close = () => {
+        console.log("close");
+    };
+    return {
+        "write": write,
+        "close": close
+    };
+};
 
 Deno.test({
     "name": "Dummy integration test",
     "ignore": Deno.args.includes("--no-integration"),
     fn() {
-        const pipeline = coupling(
-            "file1.asm", Deno.readTextFileSync, outputFile
-        );
+        const pipeline = coupling("file1.asm", mockFileReader, mockOutputFile);
         console.log(pipeline);
-    },
+    }
 });
