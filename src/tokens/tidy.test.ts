536408f34b9d96638f0f0e76e840128e22288378
Andy Preston <edgeeffect@gmail.com>
2025-01-04 16:33:47 +0000

    the size of tokenise.test was getting out of hand

---
+++ src/tokens/tidy.test.ts
@@ -0,0 +1,43 @@
+import { assertEquals } from "assert";
+import { testLine } from "./testing.ts";
+import { tokenise } from "./tokenise.ts";
+
+Deno.test("Leading and trailing whitespace is discarded", () => {
+    const line = testLine("\tLDI R16, 23   ");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "LDI");
+    assertEquals(tokenised.symbolicOperands, ["R16", "23"]);
+});
+
+Deno.test("Lines could be entirely blank", () => {
+    const line = testLine("");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "");
+    assertEquals(tokenised.symbolicOperands, []);
+});
+
+Deno.test("Multiple spaces are reduced to one space", () => {
+    const line = testLine("LDI     R16, \t 23");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "LDI");
+    assertEquals(tokenised.symbolicOperands, ["R16", "23"]);
+});
+
+Deno.test("Comments are stripped and discarded", () => {
+    const line = testLine("LDI R16, 23 ; Put 16 in R16");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "LDI");
+    assertEquals(tokenised.symbolicOperands, ["R16", "23"]);
+});
+
+Deno.test("A line could be just a comment", () => {
+    const line = testLine("; Just a comment");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "");
+    assertEquals(tokenised.symbolicOperands, []);
+});
---
+++ src/tokens/upper-case.test.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "assert";
+import { testLine } from "./testing.ts";
+import { tokenise } from "./tokenise.ts";
+
+Deno.test("Mnemonics are automatically converted to upper case", () => {
+    const line = testLine("ldi R16, \t 23");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "LDI");
+    assertEquals(tokenised.symbolicOperands, ["R16", "23"]);
+});
+
+Deno.test("... but operands aren't", () => {
+    const line = testLine("ldi _register, \t 23");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.label, "");
+    assertEquals(tokenised.mnemonic, "LDI");
+    assertEquals(tokenised.symbolicOperands, ["_register", "23"]);
+});
+
+Deno.test("... unless they are register names", () => {
+    const line = testLine("ldi r16, 23");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.symbolicOperands, ["R16", "23"]);
+});
+
+Deno.test("... or index register names", () => {
+    const line = testLine("ldi x, 23");
+    const tokenised = tokenise(line);
+    assertEquals(tokenised.symbolicOperands, ["X", "23"]);
+});
+
