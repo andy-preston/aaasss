ea8f789b4d5785e03ae2279ccc4a71e8e7af8d00
Andy Preston <edgeeffect@gmail.com>
2025-01-05 22:01:02 +0000

    I must've got over-excited

--- src/instructions/des.ts
+++ src/instructions/des.ts
@@ -3,13 +3,13 @@ import type { Code } from "../object-code/data-types.ts";
 import { lineWithObjectCode, type LineWithPokedBytes } from "../object-code/line-types.ts";
 import type { EncodedInstruction } from "../object-code/object-code.ts";
 import { template } from "../object-code/template.ts";
-import { validOperands } from "../operands/valid-and-scaled.ts";
+import { validScaledOperands } from "../operands/valid-and-scaled.ts";
 
 export const des = (
     line: LineWithPokedBytes
 ): EncodedInstruction | undefined => {
     const codeGenerator = (_device: DevicePropertiesInterface) => {
-        const operands = validOperands(line, 1);
+        const operands = validScaledOperands(line, 1);
         const operand = operands("nybble", 0);
         const code: Code = template("1001_0100 KKKK_1011", [
             ["K", operand]
