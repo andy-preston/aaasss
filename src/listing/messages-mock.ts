0b5453b38806dfe28d0d6f36856dcf572649629d
Andy Preston <edgeeffect@gmail.com>
2025-04-04 23:36:17 +0100

    Made listing/message-mock a bit "prettier"

--- src/listing/messages-mock.ts
+++ src/listing/messages-mock.ts
@@ -3,41 +3,20 @@ import { LineWithObjectCode } from "../object-code/line-types.ts";
 
 export const mockFailureMessages = (
     failure: Failure, _line: LineWithObjectCode
-) => {
-    const result: Array<string> = [failure.kind];
-
-    const push = (label: string, value: string) => {
-        if (value != undefined) {
-            result.push(`${label}: ${value}`);
-        }
-    }
-
-    for (const property in failure) {
-        if (["kind", "onOperand"].includes(property)) {
-            continue;
-        }
-
-        // deno-lint-ignore no-explicit-any
-        const value = (failure as any)[property];
-        if (Array.isArray(value)) {
-            // deno-lint-ignore no-explicit-any
-            for (const [index, item] of (value as Array<any>).entries()) {
-                push(`${property}[${index}]`, item);
-            }
-            continue;
-        }
-
-        if (value instanceof Object) {
-            for (const littleProperty in value) {
-            // deno-lint-ignore no-explicit-any
-            const littleValue = (value as any)[littleProperty];
-                push(`${property}.${littleProperty}`, littleValue);
-
-            }
-            continue;
-        }
-
-        push(property, value);
-    }
-    return result;
-};
+) => ([ failure.kind ] as Array<string>).concat(
+    Object.entries(failure).filter(
+        ([label, value]) => label != "kind" && value != undefined
+    ).flatMap(([label, value]) =>
+        Array.isArray(value)
+        ? value.map(
+            (item, index) => `${label}[${index}]: ${item}`
+        )
+        : value instanceof Object
+        ? Object.entries(value).filter(
+            ([_field, value]) =>  value != undefined
+        ).map(
+            ([field, value]) => `${label}.${field}: ${value}`
+        )
+        : `${label}: ${value}`
+    )
+);
