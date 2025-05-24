a77e3dc0d4527e0532c71155c1b97240cd62216d
Andy Preston <edgeeffect@gmail.com>
2025-05-24 16:47:43 +0100

    WIP - Simplification binge

--- src/assembler/testing.ts
+++
@@ -1,6 +0,0 @@
-import { line } from "../line/line-types.ts";
-
-export const mockNextPass = function* () {
-    yield line("", 0, "", "", 0, true).withPass(1);
-    yield line("", 0, "", "", 0, false).withPass(2);
-};
--- src/failure/line-types.ts
+++
@@ -1,7 +0,0 @@
-import type { ImmutableLine } from "../line/line-types.ts";
-
-export interface LineWithFailures {
-    "failures": ImmutableLine["failures"];
-    "withFailures": ImmutableLine["withFailures"];
-    "failed": ImmutableLine["failed"];
-};
