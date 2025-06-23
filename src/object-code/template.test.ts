f92d01252d4a4da972abf00a2544edf27a8d757f
Andy Preston <edgeeffect@gmail.com>
2025-06-23 18:29:17 +0100

    Template test to confirm thoughts for Index-Indirect Addressing

--- src/object-code/template.test.ts
+++ src/object-code/template.test.ts
@@ -1,6 +1,9 @@
 import { expect } from "jsr:@std/expect";
 import { template } from "./template.ts";
 
+const asBinary = (bytes: Array<number>) =>
+    bytes.map(byte => byte.toString(2).padStart(8, "0"));
+
 Deno.test("A plain binary number is rendered as numbers", () => {
     const result = template("1111_0000 0000_1111", {});
     expect(result).toEqual([0xf0, 0x0f]);
@@ -9,4 +12,19 @@ Deno.test("A plain binary number is rendered as numbers", () => {
 Deno.test("Substitutions are substituted", () => {
     const result = template("dddd_ssss ssss_dddd", {"d": 0xff, "s": 0x00});
     expect(result).toEqual([0xf0, 0x0f]);
+});
+
+Deno.test("Bit order of substitutions is 'natural'", () => {
+    const result = asBinary(
+        template("dddd_dddd 0000_0000", {"d": 0b01010011})
+    );
+    expect(result[0]).toBe("01010011");
+});
+
+Deno.test("Bit order of substitutions is 'natural' across byte boundaries", () => {
+    const result = asBinary(
+        template("1111_dddd dddd_0000", {"d": 0b01010011})
+    );
+    expect(result[0]).toBe("11110101");
+    expect(result[1]).toBe("00110000");
 });
