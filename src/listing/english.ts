0c81e31521488e76802411b3361dc962ff3c6323
Andy Preston <edgeeffect@gmail.com>
2025-06-23 09:10:24 +0100

    removed comment from english.ts

--- src/listing/english.ts
+++ src/listing/english.ts
@@ -142,12 +142,6 @@ export const messages: Record<FailureKind, FailureMessage> = {
     "syntax_invalidMnemonic": (failure) => withLocation(failure, [
         "A mnemonic should only contain letters"
     ]),
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    // All of these `type_*` failures should go too
-    // `value_*` is better
-    //
-    ////////////////////////////////////////////////////////////////////////////
     "type_relativeAddress": (failure) => withLocation(failure, [
         "Relative address"
     ]),
