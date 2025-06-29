eb2dc75f8313969f4c35f2a8923ee0267be384e3
Andy Preston <edgeeffect@gmail.com>
2025-06-29 12:45:17 +0100

    operand and code generation testing

--- src/object-code/encoder.test.ts
+++ src/object-code/encoder.test.ts
@@ -1,13 +1,11 @@
-import type { BoringFailure, SupportFailure } from "../failure/bags.ts";
-
 import { expect } from "jsr:@std/expect";
 import { testSystem } from "./testing.ts";
 
 Deno.test("Lines with no mnemonic don't bother generating code", () => {
     const systemUnderTest = testSystem();
     systemUnderTest.objectCode.line();
-    expect(systemUnderTest.currentLine().failures.length).toBe(0);
-    expect(systemUnderTest.currentLine().code.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(systemUnderTest.currentLine().code).toEqual([]);
     expect(systemUnderTest.programMemory.address()).toBe(0);
 });
 
@@ -16,12 +14,15 @@ Deno.test("Attempting to generate code with no device selected fails", () => {
     systemUnderTest.currentLine().mnemonic = "DES";
     systemUnderTest.currentLine().operands = ["1"];
     systemUnderTest.objectCode.line();
-    const failures = systemUnderTest.currentLine().failures;
-    expect(failures.length).toBe(3);
-    expect(failures[0]!.kind).toBe("device_notSelected");
-    expect(failures[1]!.kind).toBe("mnemonic_supportedUnknown");
-    expect(failures[2]!.kind).toBe("programMemory_sizeUnknown");
-    expect(systemUnderTest.currentLine().code.length).toBe(1);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "device_notSelected", "location": undefined
+    }, {
+        "kind": "mnemonic_supportedUnknown", "location": undefined,
+        "clue": "DES"
+    }, {
+        "kind": "programMemory_sizeUnknown", "location": undefined
+    }]);
+    expect(systemUnderTest.currentLine().code).toEqual([[0x1b, 0x94]]);
 });
 
 Deno.test("Lines with unsupported instructions fail", () => {
@@ -30,14 +31,11 @@ Deno.test("Lines with unsupported instructions fail", () => {
     systemUnderTest.instructionSet.unsupportedGroups(["DES"]);
     systemUnderTest.currentLine().mnemonic = "DES";
     systemUnderTest.objectCode.line();
-    const failures = systemUnderTest.currentLine().failures;
-    expect(failures.length).toBe(1);
-    const failure = failures[0] as SupportFailure;
-    expect(failure.kind).toBe("notSupported_mnemonic");
-    expect(failure.used).toBe("DES");
-    expect(failure.suggestion).toBe(undefined);
-    expect(failure.location).toBe(undefined);
-    expect(systemUnderTest.currentLine().code.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "notSupported_mnemonic", "location": undefined,
+        "used": "DES", "suggestion": undefined
+    }]);
+    expect(systemUnderTest.currentLine().code).toEqual([]);
 });
 
 Deno.test("Lines with unknown instructions fail", () => {
@@ -45,11 +43,11 @@ Deno.test("Lines with unknown instructions fail", () => {
     systemUnderTest.symbolTable.deviceSymbol("deviceName", "plop");
     systemUnderTest.currentLine().mnemonic = "NOT_REAL";
     systemUnderTest.objectCode.line();
-    const failures = systemUnderTest.currentLine().failures;
-    expect(failures.length).toBe(1);
-    const failure = failures[0] as BoringFailure;
-    expect(failure.kind).toBe("mnemonic_unknown");
-    expect(systemUnderTest.currentLine().code.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "mnemonic_unknown", "location": undefined,
+        "clue": "NOT_REAL"
+    }]);
+    expect(systemUnderTest.currentLine().code).toEqual([]);
 });
 
 Deno.test("Lines with real/supported instructions produce code", () => {
@@ -60,7 +58,7 @@ Deno.test("Lines with real/supported instructions produce code", () => {
     systemUnderTest.currentLine().mnemonic = "DES";
     systemUnderTest.currentLine().operands = ["15"];
     systemUnderTest.objectCode.line();
-    expect(systemUnderTest.currentLine().failures.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
     expect(systemUnderTest.currentLine().code).toEqual([[0xfb, 0x94]]);
     expect(systemUnderTest.programMemory.address()).toBe(1);
 });
@@ -72,8 +70,8 @@ Deno.test("If a line has `isDefiningMacro == true`, no code is generated", () =>
     systemUnderTest.currentLine().operands = ["15"];
     systemUnderTest.currentLine().isDefiningMacro = true;
     systemUnderTest.objectCode.line();
-    expect(systemUnderTest.currentLine().failures.length).toBe(0);
-    expect(systemUnderTest.currentLine().code.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(systemUnderTest.currentLine().code).toEqual([]);
     expect(systemUnderTest.programMemory.address()).toBe(0);
 });
 
@@ -84,7 +82,7 @@ Deno.test("Generating code will increment the programMemory address", () => {
     systemUnderTest.currentLine().mnemonic = "NOP";
     expect(systemUnderTest.programMemory.address()).toBe(0);
     systemUnderTest.objectCode.line();
-    expect(systemUnderTest.currentLine().failures.length).toBe(0);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
     expect(systemUnderTest.currentLine().code).toEqual([[0, 0]]);
     expect(systemUnderTest.programMemory.address()).toBe(1);
 });
@@ -96,10 +94,10 @@ Deno.test("Insufficient program memory causes generation to fail", () => {
     const preFailureAddress = systemUnderTest.programMemory.address();
     systemUnderTest.currentLine().mnemonic = "NOP";
     systemUnderTest.objectCode.line();
-    const failures = systemUnderTest.currentLine().failures;
-    expect(failures.length).toBe(1);
-    const failure = failures[0]!;
-    expect(failure.kind).toBe("programMemory_outOfRange");
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "programMemory_outOfRange", "location": undefined,
+        "expected": "0", "actual": "1"
+    }]);
     // Code is still generated
     expect(systemUnderTest.currentLine().code).toEqual([[0, 0]]);
     // But the address doesn't advance
--- src/operands/operands.test.ts
+++ src/operands/operands.test.ts
@@ -34,13 +34,11 @@ Deno.test("Line must have at least the expected register operands", () => {
     systemUnderTest.currentLine().operands = ["Z"];
     systemUnderTest.operands(["onlyZ", "register"]);
     expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "operand_count",
-        "expected": "2",  "actual": "1",
-        "location": undefined
+        "kind": "operand_count", "location": undefined,
+        "expected": "2",  "actual": "1"
     }, {
-        "kind": "register_notFound",
-        "clue": "",
-        "location": {"operand": 2}
+        "kind": "register_notFound", "location": {"operand": 2},
+        "clue": ""
     }]);
 });
 
@@ -49,13 +47,11 @@ Deno.test("Line must have at least the expected numeric operands", () => {
     systemUnderTest.currentLine().operands = ["Z"];
     systemUnderTest.operands(["onlyZ", "6BitNumber"]);
     expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "operand_count",
-        "expected": "2",  "actual": "1",
-        "location": undefined
+        "kind": "operand_count", "location": undefined,
+        "expected": "2",  "actual": "1"
     }, {
-        "kind": "value_type",
-        "expected": "6BitNumber", "actual": "string: ()",
-        "location": {"operand": 2}
+        "kind": "value_type", "location": {"operand": 2},
+        "expected": "6BitNumber", "actual": "string: ()"
     }]);
 });
 
@@ -74,13 +70,11 @@ Deno.test("line and expectation types must not be different", () => {
     systemUnderTest.currentLine().operands = ["R10", "R12"];
     systemUnderTest.operands(["nybble", "ioPort"]);
     expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "value_type",
-        "expected": "nybble", "actual": "string: (register: R10)",
-        "location": {"operand": 1}
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "nybble", "actual": "string: (register: R10)"
     }, {
-        "kind": "value_type",
-        "expected": "ioPort", "actual": "string: (register: R12)",
-        "location": {"operand": 2}
+        "kind": "value_type", "location": {"operand": 2},
+        "expected": "ioPort", "actual": "string: (register: R12)"
     }]);
 });
 
@@ -92,129 +86,21 @@ Deno.test("Everything's lovely when actual and expected match", () => {
     expect(result).toEqual([10, 5]);
 });
 
-Deno.test("Some operands can be optional", () => {
+Deno.test("Some mnemonics don't have any operands at all", () => {
     const systemUnderTest = testSystem();
     systemUnderTest.currentLine().operands = [];
-    expect(systemUnderTest.operands(["optionalZ+"])).toEqual([0]);
+    systemUnderTest.operands([]);
     expect(systemUnderTest.currentLine().failures).toEqual([]);
 });
 
-Deno.test("An expression yields a value", () => {
+Deno.test("... and providing some isn't allowed", () => {
     const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["20 / 2"];
-    const result = systemUnderTest.operands(["nybble"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([]);
-    expect(result).toEqual([10]);
-});
-
-Deno.test("A register symbol yields a value", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["R7"];
-    const result = systemUnderTest.operands(["register"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([]);
-    expect(result).toEqual([7]);
-});
-
-Deno.test("A non-existant register symbol yields failures", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["R99"];
-    const result = systemUnderTest.operands(["register"]);
+    systemUnderTest.currentLine().operands = ["R5", "12"];
+    systemUnderTest.operands([]);
     expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "register_notFound",
-        "clue": "R99",
-        "location": {"operand": 1}
+        "kind": "operand_count", "location": undefined,
+        "expected": "0", "actual": "2"
     }]);
-    expect(result).toEqual([0]);
-});
-
-Deno.test("A register can't be used for a number", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["R7"];
-    const result = systemUnderTest.operands(["6BitNumber"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "value_type",
-        "expected": "6BitNumber", "actual": "string: (register: R7)",
-        "location": {"operand": 1}
-    }]);
-    expect(result).toEqual([0]);
-});
-
-Deno.test("A symbol can't be used as a register", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.symbolTable.builtInSymbol("plop", 5);
-    systemUnderTest.currentLine().operands = ["plop", "plop"];
-    const result = systemUnderTest.operands(["byte", "register"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "register_notFound",
-        "clue": "plop",
-        "location": {"operand": 2}
-    }]);
-    expect(result).toEqual([5, 0]);
-});
-
-Deno.test("'Specific Symbolic' operands yield a numeric value", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["Z+"];
-    const result = systemUnderTest.operands(["ZorZ+"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([]);
-    expect(result).toEqual([1]);
-});
-
-Deno.test("Bad 'Specific Symbolic' operands yield failures", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["plop"];
-    const result = systemUnderTest.operands(["onlyZ"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "value_type",
-        "expected": "Z", "actual": "string: (plop)",
-        "location": {"operand": 1}
-    }]);
-    expect(result).toEqual([0]);
-});
-
-Deno.test("Indirect Offset operands are extracted from a single operand with horrible syntax", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["Y+23"];
-    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([]);
-    expect(result).toEqual([1, 23]);
-});
-
-
-Deno.test("Indirect Offset operands can be an expression", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["Y+5+5"];
-    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([]);
-    expect(result).toEqual([1, 10]);
-});
-
-Deno.test("Indirect Offset doesn't use the X register", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["X+23"];
-    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "value_type",
-        "expected": "Z+, Y+", "actual": "X+23",
-        "location": {"operand": 1}
-    }]);
-    expect(result).toEqual([0, 23]);
-});
-
-Deno.test("Indirect Offset must resolve to a number", () => {
-    const systemUnderTest = testSystem();
-    systemUnderTest.currentLine().operands = ["Y+plop"];
-    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
-    expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "js_error",
-        "exception": "ReferenceError", "message": "plop is not defined",
-        "location": undefined,
-    }, {
-        "kind": "value_type",
-        "expected": "6BitOffset", "actual": "Y+plop",
-        "location": {"operand": 1},
-    }]);
-    expect(result).toEqual([1, 0]);
 });
 
 Deno.test("Optional parameters can be absent", () => {
@@ -238,11 +124,120 @@ Deno.test("Incorrect optional parameters yield a failure", () => {
     systemUnderTest.currentLine().operands = ["plop"];
     const result = systemUnderTest.operands(["optionalZ+"]);
     expect(systemUnderTest.currentLine().failures).toEqual([{
-        "kind": "value_type",
-        "expected": "undefined, Z+", "actual": "string: (plop)",
-        "location": {"operand": 1}
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "undefined, Z+", "actual": "string: (plop)"
     }]);
     expect(result).toEqual([0]);
+});
+
+Deno.test("An expression yields a value", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["20 / 2"];
+    const result = systemUnderTest.operands(["nybble"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(result).toEqual([10]);
+});
+
+Deno.test("A register symbol yields a value", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["R7"];
+    const result = systemUnderTest.operands(["register"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(result).toEqual([7]);
+});
+
+Deno.test("A non-existant register symbol yields failures", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["R99"];
+    const result = systemUnderTest.operands(["register"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "register_notFound", "location": {"operand": 1},
+        "clue": "R99"
+    }]);
+    expect(result).toEqual([0]);
+});
+
+Deno.test("A register can't be used for a number", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["R7"];
+    const result = systemUnderTest.operands(["6BitNumber"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "6BitNumber", "actual": "string: (register: R7)"
+    }]);
+    expect(result).toEqual([0]);
+});
+
+Deno.test("A symbol can't be used as a register", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.symbolTable.builtInSymbol("plop", 5);
+    systemUnderTest.currentLine().operands = ["plop", "plop"];
+    const result = systemUnderTest.operands(["byte", "register"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "register_notFound", "location": {"operand": 2},
+        "clue": "plop"
+    }]);
+    expect(result).toEqual([5, 0]);
+});
+
+Deno.test("'Specific Symbolic' operands yield a numeric value", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["Z+"];
+    const result = systemUnderTest.operands(["ZorZ+"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(result).toEqual([1]);
+});
+
+Deno.test("Bad 'Specific Symbolic' operands yield failures", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["plop"];
+    const result = systemUnderTest.operands(["onlyZ"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "Z", "actual": "string: (plop)"
+    }]);
+    expect(result).toEqual([0]);
+});
+
+Deno.test("Indirect Offset operands are extracted from a single operand with horrible syntax", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["Y+23"];
+    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(result).toEqual([1, 23]);
+});
+
+Deno.test("Indirect Offset operands can be an expression", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["Y+5+5"];
+    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([]);
+    expect(result).toEqual([1, 10]);
+});
+
+Deno.test("Indirect Offset doesn't use the X register", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["X+23"];
+    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "Z+, Y+", "actual": "X+23"
+    }]);
+    expect(result).toEqual([0, 23]);
+});
+
+Deno.test("Indirect Offset must resolve to a number", () => {
+    const systemUnderTest = testSystem();
+    systemUnderTest.currentLine().operands = ["Y+plop"];
+    const result = systemUnderTest.operands(["indexWithOffset", "6BitOffset"]);
+    expect(systemUnderTest.currentLine().failures).toEqual([{
+        "kind": "js_error", "location": undefined,
+        "exception": "ReferenceError", "message": "plop is not defined"
+    }, {
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "6BitOffset", "actual": "Y+plop"
+    }]);
+    expect(result).toEqual([1, 0]);
 });
 
 Deno.test("An uninitialised symbol yields a failure", () => {
@@ -253,9 +248,8 @@ Deno.test("An uninitialised symbol yields a failure", () => {
         "kind": "js_error",
         "exception": "ReferenceError", "message": "plop is not defined"
     }, {
-        "kind": "value_type",
-        "expected": "nybble", "actual": "string: ()",
-        "location": {"operand": 1}
+        "kind": "value_type", "location": {"operand": 1},
+        "expected": "nybble", "actual": "string: ()"
     }]);
     expect(result).toEqual([0]);
 });
