5193e2d9b58d465d2e3d305e0bec8b8d97470a31
Andy Preston <edgeeffect@gmail.com>
2025-07-17 21:03:08 +0100

    Function directives testing and fixing up

--- src/assembler/byte-operations.ts
+++ src/assembler/byte-operations.ts
@@ -1,3 +1,5 @@
 export const lowByte = (word: number) => word & 0xff;
+
 export const highByte = (word: number) => (word >> 8) & 0xff;
+
 export const complement = (byte: number) => byte < 0 ? 0x0100 + byte : byte;
--- src/directives/function-directives.ts
+++ src/directives/function-directives.ts
@@ -17,14 +17,24 @@ export const functionDirectives = (currentLine: CurrentLine) => {
         return good;
     };
 
+    const validSignedByte = (byte: number) => {
+        const good = byte >= -128 && byte <= 127;
+        if (!good) {
+            addFailure(currentLine().failures, assertionFailure(
+                "parameter_value", "(signed byte) (-128)-127", `${byte}`
+            ));
+        }
+        return good;
+    }
+
     const lowDirective = (word: number): DirectiveResult =>
         validWord(word) ? lowByte(word) : 0;
 
     const highDirective = (word: number): DirectiveResult =>
         validWord(word) ? highByte(word) : 0;
 
-    const complementDirective = (value: number): DirectiveResult =>
-        complement(value);
+    const complementDirective = (byte: number): DirectiveResult =>
+        validSignedByte(byte) ? complement(byte) : 0;
 
     return {
         "low": lowDirective,
